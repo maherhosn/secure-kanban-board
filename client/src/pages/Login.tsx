1ed6c14a727c9645544e91ccb3fe7f2c3f56d58c
maherhosn <38202267+maherhosn@users.noreply.github.com>
2024-11-18 20:55:19 -0500

    Completed the styling

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -45,7 +45,7 @@ const Login = () => {
           value={loginData.password || ''}
           onChange={handleChange}
         />
-        <button type='submit'>Submit Form</button>
+        <button type='submit'>Login</button>
       </form>
     </div>
     
